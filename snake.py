acfba1fb118f6c8caa7bd677fca9af137247d537
Sean Donnellan <darkdonno@gmail.com>
2012-01-28 16:07:24 +1030

    Added the asset for the snakes head. Changed the Snake to use the head image, as well as implemented primative flipping on direction change.

--- snake.py
+++ snake.py
@@ -1,10 +1,21 @@
 import pygame
 
+import os.path
+
+def path(*args):
+	"""Helper function for returning the path to an asset"""
+	# Example: path('subfolder', 'image.jpg')	
+	return os.path.join('assets', *args)
+
+
 class Snake(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.Surface([50,20])
-		self.image.fill((0,255,0))
+		self.imageFor = pygame.image.load(path('head.png'))
+		self.imageRev = pygame.transform.flip(self.imageFor, True, False)
+		self.image = self.imageFor
+		#self.image = pygame.Surface([50,20])
+		#self.image.fill((0,255,0))
 		self.rect = self.image.get_rect()
 		initPos = [100,100]
 		self.rect.topleft = initPos
@@ -17,6 +28,10 @@ class Snake(pygame.sprite.Sprite):
 	def move(self, x, y):
 		self.x_speed += x
 		self.y_speed += y
+		if (self.x_speed > 0):
+			self.image = self.imageRev
+		elif self.x_speed < 0:
+			self.image = self.imageFor
 
 	def update(self, timeSinceLastUpdate):
 		self.rect.top += self.y_speed
