6b0b38855a291b5abc7eab62a377f6f288e1702f
Hattiel <hattie.lloyd@gmail.com>
2012-01-28 14:11:17 +1030

    now does stuff

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 
+import pygame
 import snake
 
 class Game:
@@ -6,7 +7,26 @@ class Game:
 		self.player = snake.Snake()
 
 	def onEvent(self, event):
-		pass
+                if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_LEFT:
+                                self.player.move(-3,0)
+                        if event.key == pygame.K_RIGHT:
+                                self.player.move(3,0)
+                        if event.key == pygame.K_UP:
+                                self.player.move(0,-3)
+                        if event.key == pygame.K_DOWN:
+                                self.player.move(0,3)
+                if event.type == pygame.KEYUP:
+                        if event.key == pygame.K_LEFT:
+                                self.player.move(3,0)
+                        if event.key == pygame.K_RIGHT:
+                                self.player.move(-3,0)
+                        if event.key == pygame.K_UP:
+                                self.player.move(0,3)
+                        if event.key == pygame.K_DOWN:
+                                self.player.move(0,-3)
+		self.player.update(0)
 
 	def onRender(self, screen):
+		screen.fill((0,0,0))
 		self.player.draw(screen)	
--- snake.py
+++ snake.py
@@ -1,14 +1,26 @@
+import pygame
 
-
-class Snake:
+class Snake(pygame.sprite.Sprite):
 	def __init__(self):
-		pass
-
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface([50,20])
+		self.image.fill((0,255,0))
+		self.rect = self.image.get_rect()
+		initPos = [100,100]
+		self.rect.topleft = initPos
+		self.x_speed = 0
+		self.y_speed = 0
+"""
 	def draw(self, screen):
-		pass
+		pygame.sprite.RenderPlain((self)).draw(screen) #magic
+		pygame.display.flip()
+"""
+	def move(self, x, y):
+		self.x_speed += x
+		self.y_speed += y
 
-	def movement(self, something):
-		pass
 
 	def update(self, timeSinceLastUpdate):
-		pass
+		self.rect.top += self.y_speed
+		self.rect.left += self.x_speed
+
