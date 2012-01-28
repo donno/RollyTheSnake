cdbfa05cfe704403046dab1841e247542d583fb0
Sean Donnellan <darkdonno@gmail.com>
2012-01-28 16:43:59 +1030

    Added the end asset (the tail of the snake). Changed the Snake class to take into account the head and the tail of the snake.

--- snake.py
+++ snake.py
@@ -8,15 +8,34 @@ def path(*args):
 	return os.path.join('assets', *args)
 
 
-class Snake(pygame.sprite.Sprite):
+class Snake(pygame.sprite.Group):
 	def __init__(self):
-		pygame.sprite.Sprite.__init__(self)
-		self.imageFor = pygame.image.load(path('head.png'))
-		self.imageRev = pygame.transform.flip(self.imageFor, True, False)
-		self.image = self.imageFor
+		pygame.sprite.Group.__init__(self)
+
+		# Create the head.
+		self.head = pygame.sprite.Sprite()
+		self.head.imageToRight = pygame.image.load(path('head.png'))
+		self.head.imageToLeft = pygame.transform.flip(self.head.imageToRight, True, False)
+		self.head.image = self.head.imageToLeft
+		self.head.rect = self.head.image.get_rect()
+		# Create the tail.
+		self.tail = pygame.sprite.Sprite()
+		self.tail.imageToRight = pygame.image.load(path('end.png'))
+		self.tail.imageToLeft = pygame.transform.flip(self.tail.imageToRight, True, False)
+		self.tail.image = self.tail.imageToLeft
+		self.tail.rect = self.tail.image.get_rect()
+
+		self.add(self.tail)
+		self.add(self.head)
+
+		self.tail.x_offset = 22
+		#self.tail.rect.left += self.head.rect.width - self.tail.x_offset
+		self.tail.rect.left = self.head.rect.left - self.head.rect.width + self.tail.x_offset
+		self.tail.rect.top += 16
+
 		#self.image = pygame.Surface([50,20])
 		#self.image.fill((0,255,0))
-		self.rect = self.image.get_rect()
+		self.rect = self.head.image.get_rect()
 		initPos = [100,100]
 		self.rect.topleft = initPos
 		self.x_speed = 0
@@ -26,13 +45,26 @@ class Snake(pygame.sprite.Sprite):
 		pygame.sprite.RenderPlain((self)).draw(screen) #magic
 
 	def move(self, x, y):
+		oldSpeed = -self.x_speed
 		self.x_speed += x
 		self.y_speed += y
-		if (self.x_speed > 0):
-			self.image = self.imageRev
+		# TODO: Figure out how to flip the entire group.
+
+		if self.x_speed > 0:
+			self.head.image = self.head.imageToLeft
+			self.tail.image = self.tail.imageToLeft
+			self.tail.rect.left = self.head.rect.left - self.head.rect.width + self.tail.x_offset
 		elif self.x_speed < 0:
-			self.image = self.imageFor
+			self.head.image = self.head.imageToRight
+			self.tail.image = self.tail.imageToRight
+			self.tail.rect.left = self.head.rect.right - self.tail.x_offset
+
 
 	def update(self, timeSinceLastUpdate):
 		self.rect.top += self.y_speed
 		self.rect.left += self.x_speed
+
+
+		for sprite in self.sprites():
+			sprite.rect.top += self.y_speed
+			sprite.rect.left += self.x_speed
