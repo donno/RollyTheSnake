488f79b91053a6d3d9e8eb9d8780af35763e9da5
Sean Donnellan <darkdonno@gmail.com>
2012-01-29 10:31:26 +1030

    Removed the import of the previously deleted snake file. Corrected file indentation in game.py and now handles holding down the key to move the snake left and right.

--- game.py
+++ game.py
@@ -1,40 +1,40 @@
 
 import pygame
-import snake
-
 import engine.snake
 
 class Game:
 	def __init__(self):
-		#self.player = snake.Snake()
-                self.player = engine.snake.Snake((100, 100))
-
+		self.player = engine.snake.Snake((500, 300))
+		self.leftPressed = False
+		self.rightPressed = False
 
 	def onEvent(self, event):
-                if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_LEFT:
-                                self.player.move(0.5)
-                        if event.key == pygame.K_RIGHT:
-                                self.player.move(-0.5)
-                        if event.key == pygame.K_UP:
-                                self.player.changespeed(3)
-                        if event.key == pygame.K_DOWN:
-                                self.player.changespeed(-3)
-                elif event.type == pygame.KEYUP:
-                        if event.key == pygame.K_LEFT:
-                                self.player.move(-0.5)
-                        if event.key == pygame.K_RIGHT:
-                                self.player.move(0.5)
-                        if event.key == pygame.K_UP:
-                                self.player.changespeed(-3)
-                        if event.key == pygame.K_DOWN:
-                                self.player.changespeed(3)
-		
-        def onUpdate(self):
-                self.player.update(0)
-                #self.newPlayer.update(0)
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				self.leftPressed = True
+			if event.key == pygame.K_RIGHT:
+				self.rightPressed = True
+			if event.key == pygame.K_UP:
+				self.player.changespeed(3)
+			if event.key == pygame.K_DOWN:
+				self.player.changespeed(-3)
+		elif event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT:
+				self.leftPressed = False
+			if event.key == pygame.K_RIGHT:
+				self.rightPressed = False
+			if event.key == pygame.K_UP:
+				self.player.changespeed(-3)
+			if event.key == pygame.K_DOWN:
+				self.player.changespeed(3)
+
+	def onUpdate(self):
+		self.player.update(0)
+		if self.leftPressed:
+			self.player.move(-0.04)
+		if self.rightPressed:
+			self.player.move(0.04)
 
 	def onRender(self, screen):
 		screen.fill((0,0,0))
-		self.player.draw(screen)	
-                #self.newPlayer.draw(screen)
+		self.player.draw(screen)
