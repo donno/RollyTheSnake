bb69fe9643249238be9560e5a21215019ab3e7e3
Sean Donnellan <darkdonno@gmail.com>
2012-01-29 11:57:41 +1030

    Moved the hud rendering code from the main python file into the game.py.

--- game.py
+++ game.py
@@ -1,21 +1,54 @@
 
+__AUTHORS__ = ['Donno', 'Hattiel', 'Shrubber']
+
 import pygame
 import engine.snake
 import engine.mouse
 import random
 
+def renderHud(title):
+	fontName = pygame.font.get_default_font()
+	font = pygame.font.Font(fontName, 14)
+	colour = (0, 0, 255)
+
+	nameSurface = font.render(title, False, colour)
+	nameRect = nameSurface.get_rect()
+	authorSurface = font.render(', '.join(__AUTHORS__), False, colour)
+	authorsY = nameRect.y + nameRect.height + 5
+	authorsRect = authorSurface.get_rect()
+	authorsRect.y = authorsY
+	del authorsY
+	offset = 10
+
+	hudRect = (0, 0, 400, 38)
+	#hudBackground = pygame.Surface((400, 38))
+	#hudBackground.fill(
+
+	def _renderHud(screen):
+		nameRect.x = screen.get_rect().width - nameRect.width - offset
+		authorsRect.x = screen.get_rect().width - authorsRect.width - offset
+		screen.fill( ( 38, 38, 38, 28), hudRect, pygame.BLEND_RGBA_ADD)
+		screen.blit(nameSurface, nameRect)
+		screen.blit(authorSurface, authorsRect)
+
+	return _renderHud
+
 class Game:
-	def __init__(self):
+	def __init__(self, title):
+		self.title = title
 		self.player = engine.snake.Snake((500, 300))
 		self.leftPressed = False
 		self.rightPressed = False
 		self.screenSize = None
 		self.mice = []
+		
+
+		self.renderhud = renderHud(title)
 
 	def spawnMouse(self):
 		# TODO: Write some awesome here to figure out a good place to
 		# place the mouse.
-		
+
 		position = (400, 300) if not self.screenSize else (
 			random.randint(0, self.screenSize[0]),
 			random.randint(0, self.screenSize[1]))
@@ -59,8 +92,15 @@ class Game:
 			mouse.update()
 
 	def onRender(self, screen):
+		# Clear the whole screen
 		screen.fill((0,0,0))
+
+		# Draw the player (snake)
 		self.player.draw(screen)
 
+		# Draw the mobs (mice)
 		for mouse in self.mice:
 			mouse.draw(screen)
+
+		# Render the heads up display
+		self.renderhud(screen)
